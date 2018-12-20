9182a0d06ebeee2a32ee82a2563f614c59306cb2
Gabriel7Augustobc <36643489+Gabriel7Augustobc@users.noreply.github.com>
2018-12-20 15:40:36 -0300

    Delete Header.js

--- app/components/Header/Header.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
-
-import styles from './styles';
-
-
-
-export default Header;
